2239539f0012d02a5327c6ad4350fa54d21add42
zipperloo <elliotisaacs@gmail.com>
2025-12-09 23:34:11 +0000

    Initial commit: Only Human holding page with brain visualization and mobile responsiveness

---
+++ README.md
@@ -0,0 +1,19 @@
+# Only Human - Holding Page
+
+A simple coming soon page for Only Human.
+
+## Hosting
+
+This is a static HTML page that can be hosted on GitHub Pages or any static hosting service.
+
+To deploy on GitHub Pages:
+1. Push this repository to GitHub.
+2. Go to repository settings.
+3. Enable GitHub Pages and select the main branch.
+
+## Technologies
+
+- HTML5
+- CSS3 (inline)
+- JavaScript (inline)
+- Google Fonts (Inter)
---
+++ server.js
@@ -0,0 +1,59 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const url = require('url');
+
+const server = http.createServer((req, res) => {
+  const parsedUrl = url.parse(req.url, true);
+  let pathname = parsedUrl.pathname;
+
+  // Default to index.html for root path
+  if (pathname === '/') {
+    pathname = '/index.html';
+  }
+
+  const filePath = path.join(__dirname, pathname);
+
+  // Determine content type
+  const ext = path.extname(filePath);
+  let contentType = 'text/html';
+  switch (ext) {
+    case '.css':
+      contentType = 'text/css';
+      break;
+    case '.js':
+      contentType = 'text/javascript';
+      break;
+    case '.json':
+      contentType = 'application/json';
+      break;
+    case '.png':
+      contentType = 'image/png';
+      break;
+    case '.jpg':
+    case '.jpeg':
+      contentType = 'image/jpeg';
+      break;
+  }
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        res.writeHead(404);
+        res.end('File not found');
+      } else {
+        res.writeHead(500);
+        res.end('Server error');
+      }
+    } else {
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(data);
+    }
+  });
+});
+
+const PORT = 8000;
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+  console.log('Press Ctrl+C to stop the server');
+});
